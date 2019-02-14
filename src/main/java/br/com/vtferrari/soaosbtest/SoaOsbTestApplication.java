ba9c5d47d7cf69ec709faf00e3abc55547a9f697
vtferrari <vtferrari@hotmail.com>
2019-02-14 14:41:45 -0200

    Init project

---
+++ src/main/java/br/com/vtferrari/soaosbtest/SoaOsbTestApplication.java
@@ -0,0 +1,14 @@
+package br.com.vtferrari.soaosbtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaOsbTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaOsbTestApplication.class, args);
+	}
+
+}
+
